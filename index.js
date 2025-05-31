67a5b1eeba7d74912ba55abbbfe7cc977945b70a
robC_vW <robiecalong2@gmail.com>
2025-05-31 08:46:00 -0500

    renderEvents function creates card events and add delete eventListener

--- index.js
+++ index.js
@@ -1,99 +1,109 @@
-events = [
-  {
-    name: "is this it",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry",
-    date: "Sat, Jun 14",
-    time: "10pm",
-    location: "Stereo Live Nation",
-  },
-  {
-    name: "Comic Gate",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry",
-    date: "Sat, Jun 14",
-    time: "10pm",
-    location: "Stereo Live Nation",
-  },
-  {
-    name: "Comic Gate",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry",
-    date: "Sat, Jun 14",
-    time: "10pm",
-    location: "Stereo Live Nation",
-  },
-  {
-    name: "Comic Gate",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry",
-    date: "Sat, Jun 14",
-    time: "10pm",
-    location: "Stereo Live Nation",
-  },
-  {
-    name: "Comic Gate",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry",
-    date: "Sat, Jun 14",
-    time: "10pm",
-    location: "Stereo Live Nation",
-  },
-  {
-    name: "Comic Gate",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry",
-    date: "Sat, Jun 14",
-    time: "10pm",
-    location: "Stereo Live Nation",
-  },
-  {
-    name: "Comic Gate",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry",
-    date: "Sat, Jun 14",
-    time: "10pm",
-    location: "Stereo Live Nation",
-  },
-  {
-    name: "Comic Gate",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry",
-    date: "Sat, Jun 14",
-    time: "10pm",
-    location: "Stereo Live Nation",
-  }
-];
+const state = {
+  events: [
+    {
+      id: 1,
+      name: "Comic Gate1",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry",
+      date: "Sat, Jun 14",
+      time: "10pm",
+      location: "Stereo Live Nation",
+    },
+    {
+      id: 2,
+      name: "Comic Gate2",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry",
+      date: "Sat, Jun 14",
+      time: "10pm",
+      location: "Stereo Live Nation",
+    },
+    {
+      id: 3,
+      name: "Comic Gate3",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry",
+      date: "Sat, Jun 14",
+      time: "10pm",
+      location: "Stereo Live Nation",
+    },
+    {
+      id: 4,
+      name: "Comic Gate4",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry",
+      date: "Sat, Jun 14",
+      time: "10pm",
+      location: "Stereo Live Nation",
+    },
+    {
+      id: 5,
+      name: "Comic Gate5",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry",
+      date: "Sat, Jun 14",
+      time: "10pm",
+      location: "Stereo Live Nation",
+    },
+  ],
+  events2: []
+};
 
 
 function renderEvents() {
     const gridElem = document.querySelector('.event-grid')
+    gridElem.innerHTML = "";            //clears first
 
-    let eventCardsHTML = "";
-    console.log(events.length)
+    state.events.forEach((eventE) => {
+        const cardContainerElem = document.createElement("div")
+        cardContainerElem.classList.add("card-event-container")
+        cardContainerElem.classList.add(`js-card-event-container-${eventE.id}`)
+        // console.log(cardContainerElem)
 
-    events.forEach((event) => {
-        // console.log(event)
-
-        eventCardsHTML += `
-            <div class="event-container">
+        const eventCardHTML = `
                 <img src="./images/party.jpeg">
                 <div class="event-details">
-                    <span>Cosmic Gate</span>
-                    <span>Date</span>
-                    <span>Time</span>
-                    <span>Location</span>
-                    <span>Description: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry</span>
+                    <span>Event Name: ${eventE.name}</span>
+                    <span>Date: ${eventE.date}</span>
+                    <span>Time: ${eventE.time}</span>
+                    <span>Location: ${eventE.location}</span>
+                    <span>Description: ${eventE.description}</span>
                 </div>
                 <div class="div-delete-button">
-                    <button>DELETE</button>
+                    <button class="js-delete-button" data-event-id="${eventE.id}">DELETE</button>
                 </div>
-            </div>
         `;
-    });
+        cardContainerElem.innerHTML = eventCardHTML
+        // console.log(cardContainerElem)
 
-    // console.log(eventCardsHTML)
-    gridElem.innerHTML = eventCardsHTML;
+        gridElem.appendChild(cardContainerElem)     //rendered all cards
+        // console.log(gridElem)
+
+        //create Delete buttons and addEventListener
+        const deleteButtons = document.querySelectorAll(".js-delete-button");
+        deleteButtons.forEach((delButton) => {
+          delButton.addEventListener("click", () => {
+            const eventid = delButton.dataset.eventId;
+            // console.log(eventid)
+            removeEvent(eventid);
+            // console.log("removed successful" + eventid);
+
+            renderEvents();
+          });
+        });
+    });
+}
+
+function removeEvent(id) {
+    const newEvents = []
+
+    state.events.forEach((eventE) => {
+        if (eventE.id !== Number(id)) {
+            newEvents.push(eventE)
+        }
+    })
+
+    state.events = newEvents
 }
 
 function init() {
